23a286f3719cc9f894303e2aedcc547c82e2905a
avishkarchavle <aschavle_b20@ce.vjti.ac.in>
2022-04-17 10:35:10 +0530

    sjf

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get('/scancam', (req, res) => {
     res.render('scancam');
 })
 
-port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 })
